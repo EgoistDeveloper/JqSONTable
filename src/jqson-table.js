adce036d9c160b203aa36b164ba373ec53caeb5f
EgoistDeveloper <epsilonelittr@gmail.com>
2019-04-21 19:22:35 +0300

    bug fix
    Fixed head order and order_by actions

--- src/jqson-table.js
+++ src/jqson-table.js
@@ -15,12 +15,12 @@
             tableLoader();
         }, 1000 * 60 * 1);
 
-    var options = {
+    var options = [];
+
+    var defaultOptions = {
         ajax: {
             url: null,
-            baseUrl: null,
             urlDelimeter: '&',
-            subUrl: null,
             timeOut: 30000,
             reloadDelay: 10,
             preventReCreate: true,
@@ -36,8 +36,7 @@
             limit: 25,
             order: 'desc',
             order_by: 'date',
-            like: '',
-            total: null
+            like: ''
         },
         table: {
             search: true,
@@ -55,35 +54,35 @@
         language: null
     };
 
-    ajaxGetTable = function () {
+    ajaxGetTable = function (id) {
         var _response = null,
-            url = options.ajax.url ? options.ajax.url : options.ajax.baseUrl + options.ajax.subUrl + options.ajax.urlDelimeter + $.param(options.pagination);
+            url = options[id].ajax.url + options[id].ajax.urlDelimeter + $.param(options[id].pagination);
 
         $.ajax({
             url: url,
             type: 'GET',
-            timeOut: options.ajax.timeOut,
+            timeOut: options[id].ajax.timeOut,
             dataType: 'json',
-            async: options.ajax.async,
+            async: options[id].ajax.async,
             beforeSend: function (request) {
-                if (options.ajax.beforeSend) {
-                    options.ajax.beforeSend(request);
+                if (options[id].ajax.beforeSend) {
+                    options[id].ajax.beforeSend(request);
                 }
             },
             success: function (response) {
-                if (options.ajax.successCallback) {
-                    options.ajax.successCallback(response);
+                if (options[id].ajax.successCallback) {
+                    options[id].ajax.successCallback(response);
                 }
 
                 _response = response;
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                if (options.ajax.logError) {
+                if (options[id].ajax.logError) {
                     printError(jqXHR, textStatus, errorThrown);
                 }
 
-                if (options.ajax.errorCallback) {
-                    options.ajax.errorCallback(jqXHR, textStatus, errorThrown);
+                if (options[id].ajax.errorCallback) {
+                    options[id].ajax.errorCallback(jqXHR, textStatus, errorThrown);
                 }
 
                 if (textStatus == 'timeout' && timeOutMaxRetry < timeOutCurrentRetry) {
@@ -124,15 +123,15 @@
         return Math.abs(Math.round(diff));
     }
 
-    function setThead(href, text) {
+    function setThead(href, text, id) {
         var thead = '<th>{{{text}}}</th>';
 
         if (href) {
             thead = thead.replace('{{{text}}}', '<a href="#' + href + '">{{{text}}}</a>');
         }
 
-        if (options.language && options.language[text]) {
-            thead = thead.replace('{{{text}}}', options.language[text]);
+        if (options[id].language && options[id].language[text]) {
+            thead = thead.replace('{{{text}}}', options[id].language[text]);
         } else {
             thead = thead.replace('{{{text}}}', text.replace('_', ' '));
         }
@@ -140,39 +139,41 @@
         return thead;
     }
 
-    function setTableRow(key, value) {
+    function setTableRow(key, value, id) {
         var trow = '<tr' + (value.id ? ' data-id="' + value.id + '"' : '') + '>{{{row}}}</tr>',
             rowContent = '';
 
-        if (options.table.numbers) {
+        if (options[id].table.numbers) {
             rowContent += '<td class="font-weight-bold">#' +
-                (((options.pagination.page * options.pagination.limit) - options.pagination.limit) + key + 1) +
+                (((options[id].pagination.page * options[id].pagination.limit) - options[id].pagination.limit) + parseInt(key) + 1) +
                 '</td>';
         }
 
         if (typeof value === 'object') {
             for (var key in value) {
-                if (options.table.except.indexOf(key) === -1 && typeof value[key] === 'string') {
-                    if (options.table.rowItem) {
-                        var rowItem = options.table.rowItem(key, value);
+                if (options[id].table.except.indexOf(key) === -1 && typeof value[key] === 'string') {
+                    if (options[id].table.rowItem) {
+                        var rowItem = options[id].table.rowItem(key, value);
 
                         rowContent += '<td>' + (rowItem ? rowItem : value[key]) + '</td>';
+                    } else {
+                        rowContent += '<td>' + value[key] + '</td>';
                     }
                 }
             }
         }
 
-        if (options.table.actions) {
+        if (options[id].table.actions) {
             rowContent += '<td><div class="btn-group dropleft">' +
-                '<button type="button" class="btn btn-md btn-' + (options.table.actionColor ? options.table.actionColor : 'primary') + ' dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-                (options.language && options.language.actions ? options.language.actions : options.table.actionsText) +
+                '<button type="button" class="btn btn-md btn-' + (options[id].table.actionColor ? options[id].table.actionColor : 'primary') + ' dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
+                (options[id].language && options[id].language.actions ? options[id].language.actions : options[id].table.actionsText) +
                 '</button>' +
                 '<div class="dropdown-menu">';
 
-            options.table.actions.forEach(function (value, key) {
+            options[id].table.actions.forEach(function (value, key) {
                 rowContent +=
                     '<button data-action="' + value.action + '" class="dropdown-item dropdown-item-red" type="button"><i class="' + value.icon + '"></i>' +
-                    (options.language ? options.language[value.text] : value.text) +
+                    (options[id].language ? options[id].language[value.text] : value.text) +
                     '</button>';
             });
 
@@ -183,43 +184,46 @@
         return trow.replace('{{{row}}}', rowContent);
     }
 
-    function tableBuilder(response) {
+    function tableBuilder(results, id) {
         var table = '<thead><tr>';
 
-        if (response && response.results) {
-            if (response.results[0]) {
-                if (options.table.numbers && options.language) {
-                    table += '<th>' + options.language.num + '</th>';
-                } else if (options.table.numbers && !options.language) {
-                    table += '<th>' + options.table.numText + '</th>';
-                }
+        if (results && results[0]) {
+            if (options[id].table.numbers && options[id].language) {
+                table += '<th>' + options[id].language.num + '</th>';
+            } else if (options[id].table.numbers && !options[id].language) {
+                table += '<th>' + options[id].table.numText + '</th>';
+            }
 
-                if (options.table.columns) {
-                    options.table.columns.forEach(function (value, key) {
-                        if (options.table.except.indexOf(value.text) === -1 && typeof value.text === 'string') {
-                            table += setThead(value.href, value.text);
-                        }
-                    });
-                } else {
-                    response.results[0].forEach(function (value, key) {
-                        if (options.table.except.indexOf(key) === -1 && typeof value === 'string') {
-                            table += setThead(key, key);
-                        }
-                    });
-                }
+            if (options[id].table.columns && options[id].table.columns.length) {
+                options[id].table.columns.forEach(function (value, key) {
+                    if (options[id].table.except.indexOf(value.text) === -1 && typeof value.text === 'string') {
+                        table += setThead(value.href, value.text, id);
+                    }
+                });
+            } else {
+                if (typeof results[0] === 'object') {
 
-                if (options.table.numbers && options.language) {
-                    table += '<th>' + options.language.actions + '</th>';
-                } else if (options.table.numbers && !options.language) {
-                    table += '<th>' + options.table.actionsText + '</th>';
+                    for (var key in results[0]) {
+                        if (options[id].table.except.indexOf(key) === -1 && typeof results[0][key] === 'string') {
+                            table += setThead(key, key, id);
+                        }
+                    }
                 }
+            }
+
+            if (options[id].table.actions && options[id].language) {
+                table += '<th>' + options[id].language.actions + '</th>';
+            } else if (options[id].table.actions && !options[id].language) {
+                table += '<th>' + options[id].table.actionsText + '</th>';
             }
 
             table += '</tr></thead><tbody>'
 
-            response.results.forEach(function (value, key) {
-                table += setTableRow(key, value);
-            });
+            if (typeof results === 'object') {
+                for (var key in results) {
+                    table += setTableRow(key, results[key], id);
+                }
+            }
         }
         return table + '</tbody>';
     }
@@ -255,12 +259,14 @@
         }
 
         // middle pages
-        for (var i = start; i < last; i++) {
-            if (i <= 10) {
-                if (page == i) {
-                    pagination += '<button data-index="' + i + '" class="btn btn-default active" title="' + lang.ui_page + (i) + '">' + i + '</button>';
-                } else if (i > 0) {
-                    pagination += '<button data-index="' + i + '" class="btn btn-default" title="' + lang.ui_page + (i) + '">' + i + '</button>';
+        for (var i = 0; i < last; i++) {
+            var _page = start+i;
+
+            if (i <= 10 && page != 1) {
+                if (_page == page) {
+                    pagination += '<button data-index="' + _page + '" class="btn btn-default active" title="' + lang.ui_page + (_page) + '">' + _page + '</button>';
+                } else if (_page != page) {
+                    pagination += '<button data-index="' + _page + '" class="btn btn-default" title="' + lang.ui_page + (_page) + '">' + _page + '</button>';
                 }
             }
         }
@@ -289,20 +295,25 @@
      * Universal table load and update controller function
      * 
      */
-    function tableLoader(initial_load, table) {
-        if (initial_load || diffMinutes(new Date(), tableLastUpdate) >= options.ajax.reloadDelay) {
-            if (table) {
-                loadTable(table);
-            } else {
-                var _tables = $('table[id="jqson-table"]');
+    function tableLoader(initial_load, table, id) {
+        if (table) {
+            if (initial_load || diffMinutes(new Date(), tableLastUpdate) >= options[id].ajax.reloadDelay) {
+                loadTable(table, id);
+            }
+        } else {
+            var _tables = $('table[id="jqson-table"]');
 
-                if (_tables.length) {
-                    _tables.forEach(function (value, key) {
-                        loadTable($(this));
-                    });
-                } else {
-                    clearInterval(tableUpdateLoop);
-                }
+            if (_tables.length) {
+                _tables.forEach(function () {
+                    var _this = $(this),
+                        id = _this.attr('id');
+
+                    if (initial_load || diffMinutes(new Date(), tableLastUpdate) >= options[id].ajax.reloadDelay) {
+                        loadTable(_this, id);
+                    }
+                });
+            } else {
+                clearInterval(tableUpdateLoop);
             }
         }
     }
@@ -310,54 +321,71 @@
     $(document).on('click', '.jqson-table thead th a', function (e) {
         e.preventDefault();
 
-        var _order_by = $(this).attr('href'),
-            _order = $(this).attr('data-order');
-        order_by = _order_by.replace('#', '');
+        var _this = $(this),
+            order_by = _this.attr('href').replace('#', ''),
+            order = _this.attr('data-order'),
+            table = _this.parents('.jqson-table'),
+            id = table.attr('id');
 
-        if (_order === undefined || _order == 'desc') {
-            tableLoader(true);
+            options[id].pagination.order_by = order_by;
 
-            var _head = $('a[href="' + _order_by + '"]');
-            _head.attr('data-order', 'asc').html(_head.text() + ' <i class="fas fa-angle-double-up"></i>');
-        } else if (_order == 'asc') {
-            tableLoader(true);
+        if (order === undefined || order == 'desc') {
+            options[id].pagination.order = 'asc';
+            tableLoader(true, table, id);
 
-            var _head = $('a[href="' + _order_by + '"]');
-            _head.attr('data-order', 'desc').html(_head.text() + ' <i class="fas fa-angle-double-down"></i>');
+            var head = $('a[href="#' + order_by + '"]');
+            head.attr('data-order', 'asc').html(head.text() + ' <i class="fas fa-angle-double-up"></i>');
+        } else if (order == 'asc') {
+            options[id].pagination.order = 'desc';
+            tableLoader(true, table, id);
+
+            var head = $('a[href="#' + order_by + '"]');
+            head.attr('data-order', 'desc').html(head.text() + ' <i class="fas fa-angle-double-down"></i>');
         }
     });
 
-    loadTable = function (table) {
-        if (options.ajax && options.ajax.baseUrl) {
-            options.ajax.subUrl = table.attr('data-path')
-            var response = ajaxGetTable();
+    loadTable = function (table, id) {
+        if (options[id].ajax) {
+            options[id].ajax.subUrl = table.attr('data-path')
+            var response = ajaxGetTable(id);
 
             if (response) {
-                if (options.ajax.preventReCreate && !jsonEqual(previousResponse, response)) {
-                    table.html(options.loader);
+                if (options[id].ajax.preventReCreate && !jsonEqual(previousResponse, response)) { // ###
+                    table.html(options[id].loader);
 
-                    var tableHtml = tableBuilder(response);
-                    setTimeout(() => {
-                        table.html(tableHtml);
-                    }, 200);
+                    var tableHtml = tableBuilder(response.results, id);
+                    table.html(tableHtml);
+                        
+                    var pagination =  getPagination(response.page, response.start, response.end, response.last);
+                    table.next('.pagination').html(pagination);
 
                     previousResponse = response;
-                } else if (!options.ajax.preventReCreate) {
-                    table.html(options.loader);
+                } else if (!options[id].ajax.preventReCreate) {
+                    table.html(options[id].loader);
 
-                    var tableHtml = tableBuilder(response);
+                    var tableHtml = tableBuilder(response, id);
                     table.html(tableHtml);
+
+                    var pagination =  getPagination(response.page, response.start, response.end, response.last);
+                    table.next('.pagination').html(pagination);
                 }
             }
+        } else {
+            // ###
         }
     }
 
     $.fn.jqSonTable = function (customOptions) {
-        if (customOptions) {
-            $.extend(true, options, customOptions);
-        }
+        var _this = $(this),
+            id = _this.attr('id');
 
-        tableLoader(true, $(this));
+        if (_this.length) {
+            if (customOptions && !options[id]) {
+                options[id] = $.extend(true, {}, defaultOptions, customOptions);
+            }
+
+            tableLoader(true, _this, id);
+        }
     };
 
     $.fn.jqSonTableReload = function () {
